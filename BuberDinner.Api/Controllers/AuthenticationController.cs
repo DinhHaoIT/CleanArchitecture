dafe6d1362ae4d733b41acb07b16578a6de7fad9
DESKTOP-9ARMAE9\dinhh <nguyendinhhao.it@gmail.com>
2024-07-21 18:49:36 +0700

    Init commit

---
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,44 @@
+﻿using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Contract.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequestContract register)
+        {
+            var registerResult = _authenticationService.Register(register.FirstName, register.LastName, register.Email, register.Password);
+            var authResponse = new AutResultContract(
+                registerResult.Id,
+                registerResult.FirstName,
+                registerResult.LastName,
+                registerResult.Email,
+                registerResult.Token
+            );
+            return Ok(authResponse);
+        }
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequestContract login) 
+        {
+            var loginResult = _authenticationService.Login(login.Email, login.Password);
+            var authResponse = new AutResultContract(
+               loginResult.Id,
+               loginResult.FirstName,
+               loginResult.LastName,
+               loginResult.Email,
+               loginResult.Token
+           );
+            return Ok(authResponse);
+        }
+    }
+}
---
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberDinner.Application.Services.Authentication
+{
+    internal class AuthenticationService : IAuthenticationService
+    {
+        public AuthenticationResult Login(string Email, string Password)
+        {
+            return new AuthenticationResult(
+                Guid.Empty,
+                "FirstName",
+                "LastName",
+                Email,
+                "token_"+Guid.NewGuid().ToString()
+            );
+        }
+
+        public AuthenticationResult Register(string FirstName, string LastName, string Email, string Password)
+        {
+            return new AuthenticationResult(
+                Guid.NewGuid(),
+                FirstName,
+                LastName,
+                Email,
+                "token_"+Guid.NewGuid().ToString()
+            );
+        }
+    }
+}
